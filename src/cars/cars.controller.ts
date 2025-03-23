4168fe00caf92bb362ac14cbccdc6301b8296eb7
camilo28k <hcbarrera-2023a@corhuila.edu.co>
2025-03-23 18:17:32 -0500

    Primer commit

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,51 @@
+ import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(
+        private readonly carsService: CarsService
+    ){}
+
+    @Get()
+    getAllCars(){
+        return this.carsService.findAll()
+    }
+
+    @Get('id/:id')
+    getCarById(@Param('id', ParseUUIDPipe)id:string){
+        console.log({id})
+        //throw new Error('Auxilio!')
+        return this.carsService.findOneById(id);
+    }
+
+    @Get('brand/:brand')
+    getCarByBrand(@Param('brand')brand){
+        console.log({brand})
+        return this.carsService.findOneByBrand(brand);
+    }
+
+
+    @Post()
+    //@UsePipes(ValidationPipe)
+    createCar(@Body() createCarDto:CreateCarDto){
+        return this.carsService.create(createCarDto);
+    }
+
+    @Patch('id/:id')
+    updateCar(
+        @Param('id', ParseIntPipe) id:number,
+        @Body() body:any){
+        return body;
+    }
+
+    @Delete('id/:id')
+    deleteCar(@Param('id', ParseIntPipe) id:number){
+        return{
+            method:'delete',
+            id
+        }
+    }
+}
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import {v4 as uuid} from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
+
+@Injectable()
+export class CarsService {
+    private cars: Car[] = [
+        {
+            id:uuid(),
+            brand: 'Honda',
+            model:'Corolla',
+        },
+        {
+            id:uuid(),
+            brand: 'Honda',
+            model:'Civic'
+        },
+        {
+            id:uuid(),
+            brand: 'Hyundai',
+            model:'Creta'
+        }
+    ];
+
+    findAll(){
+        return this.cars
+    }
+
+    findOneById(id:string){
+        const car = this.cars.find(car => car.id===id);
+
+        if(!car){
+            throw new NotFoundException(`Datos con Id '${id}' no existe!`);
+        }
+
+        return car;
+    }
+
+    findOneByBrand(brand:string){
+        const br = this.cars.filter(car => car.brand===brand);
+
+        return br;
+    }
+
+    create(createCarDto: CreateCarDto){
+        
+        const car: Car={
+            id: uuid(), //...createCarDto
+            model:  createCarDto.model,
+            brand: createCarDto.brand
+        }
+
+        this.cars.push(car);
+
+        return car;
+    }
+
+}
